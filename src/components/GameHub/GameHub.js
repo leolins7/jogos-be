785b884614dabc058bcd4f31bc5e85dc46990cf3
leolins77 <lfdln@cesar.school>
2025-08-19 15:40:41 -0300

    game hub - 2

--- src/components/GameHub/GameHub.js
+++ src/components/GameHub/GameHub.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import beLogo from '../assets/logo.png';
+import beLogo from '../assets/logo.png'; 
 import LogoutButton from '../LogoutButton/LogoutButton';
 import './GameHub.css';
 
 const GameHub = () => {
-    return (
-        <div className="games-container">
-            <Link to="/home" className="home-button"></Link>
-            <LogoutButton />
-            <img src={beLogo} alt="Be Eventos Logo" className="be-logo" />
-            <h1 className="games-title">Central de Jogos</h1>
-            <p className="games-subtitle">Selecione um jogo para começar:</p>
-            <div className="game-list">
-                <div className="game-card">
-                    <Link to="/jogo-do-perfil">Jogo do Perfil</Link>
-                </div>
-                <div className="game-card">
-                    <Link to="/jogo-da-memoria">Jogo da Memória</Link>
-                </div>
-                <div className="game-card">
-                    <Link to="/acerte-ou-saia">Acerte ou Saia</Link>
-                </div>
-                <div className="game-card">
-                    <Link to="/jogo-da-roleta">Roleta</Link>
-                </div>
-            </div>
-        </div>
-    );
+  return (
+    <div className="game-hub-container">
+      <Link to="/home" className="home-button"></Link>
+      <LogoutButton />
+      <img src={beLogo} alt="Be Eventos Logo" className="hub-logo" />
+      <h1 className="hub-title">Central de Jogos</h1>
+      <p className="hub-subtitle">Selecione um jogo para começar:</p>
+      <div className="games-list">
+        <Link to="/jogo-do-perfil" className="game-card">
+          Jogo do Perfil
+        </Link>
+        <Link to="/jogo-da-memoria" className="game-card">
+          Jogo da Memória
+        </Link>
+        <Link to="/acerte-ou-saia" className="game-card">
+          Acerte ou Saia
+        </Link>
+        <Link to="/jogo-da-roleta" className="game-card">
+          Jogo da Roleta
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default GameHub;
